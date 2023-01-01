80819893840af4882e8bfab70f426d35a8aede83
eunbinnie <dmsqlsdl54@naver.com>
2023-01-01 22:59:50 +0900

    regexp method(test, replace)

--- main.js
+++ main.js
@@ -6,6 +6,30 @@ The quick brown fox jumps over the lazy dog.
 abbcccdddd
 `;
 
-// const regexp = new RegExp('the', 'gi');
-const regexp = /the/gi;
-console.log(str.match(regexp));
+
+// 정규표현식 생성
+{
+
+  // const regexp = new RegExp('the', 'gi');
+  const regexp = /the/gi;
+  console.log(str.match(regexp));
+
+}
+
+
+// 메소드
+{
+
+  const regexp = /fox/gi;
+  console.log(regexp.test(str));  // true
+
+  const regexp2 = /fox/gi;
+  console.log(str.replace(regexp2, 'AAA')); // The quick brown AAA jumps over the lazy dog.
+  console.log(str); // The quick brown fox jumps over the lazy dog.
+
+  // let str= ~
+  // str=str.replace(regexp2,'AAA')
+  // console.log(str)
+  // : 이러면 원본 수정됨
+
+}
