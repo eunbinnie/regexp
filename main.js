067cb405b56f919a6f02126df87346afb0af93cd
eunbinnie <dmsqlsdl54@naver.com>
2023-01-02 21:05:57 +0900

    RegExp Finish!

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ The quick brown fox jumps over the lazy dog.
 abbcccdddd
 hxyp
 http://localhost:1234
+동해물과_백두산이 마르고 닳도록
 d`;
 
 
@@ -64,5 +65,37 @@ console.log("====================");
   console.log(str.match(/d{2,}/g));
   console.log(str.match(/d{2,3}/g));
   console.log(str.match(/\b\w{2,3}\b/g));
+
+}
+console.log("====================");
+
+
+const h = `  the hello  world   !
+
+
+`;
+const str2 = `
+010-1234-5678
+thesecon@gmail.com
+https://www.omdbapi.com/?apikey=7035c60c&s=frozen
+The quick brown fox jumps over the lazy dog.
+abbcccdddd
+`;
+
+// 패턴(표현)_2
+{
+
+  console.log(str.match(/[fox]/g)); // f or o or x
+  console.log(str.match(/[0-9]{1,}/g));
+  console.log(str.match(/[가-힇]{1,}/g));
+
+  console.log(str.match(/\w/g));
+  console.log(str.match(/\bf\w{1,}\b/g));
+  console.log(str.match(/\d{1,}/g));
+  console.log(str.match(/\s/g));
+  console.log(h.replace(/\s/g, ''));
+
+  console.log(str2.match(/.{1,}(?=@)/g));
+  console.log(str2.match(/(?<=@).{1,}/g));
 
 }
